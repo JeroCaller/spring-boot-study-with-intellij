df00e4b0fd8f6e6722b9428990c8eb76089337cc
JeroCaller <j69m17@gmail.com>
2026-05-04 06:55:20 +0900

    refactor: JwtExceptionHandlerFactory 클래스를 singleton에서 일반 클래스로 구조 변경.
    - 설정 클래스에서 singleton bean으로 등록하는 과정을 이미 거치기에 불필요하다고 판단하여 일반 클래스로 변경.

--- AuthTokenSecurity/src/main/java/com/jerocaller/AuthTokenSecurity/jwt/JwtExceptionHandlerFactory.java
+++ AuthTokenSecurity/src/main/java/com/jerocaller/AuthTokenSecurity/jwt/JwtExceptionHandlerFactory.java
@@ -1,22 +1,13 @@
 package com.jerocaller.AuthTokenSecurity.jwt;
 
 import io.jsonwebtoken.JwtException;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JwtExceptionHandlerFactory {
-    private static final JwtExceptionHandlerFactory jwtExceptionHandlerFactory =
-        new JwtExceptionHandlerFactory();
     private static final Map<Class<? extends JwtException>, JwtExceptionHandler> handlerMapper =
         new HashMap<>();
-
-    public static JwtExceptionHandlerFactory getInstance() {
-        return jwtExceptionHandlerFactory;
-    }
 
     public void setDefaultHandler(JwtExceptionHandler handler) {
         handlerMapper.put(JwtException.class, handler);
